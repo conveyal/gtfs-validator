74af0206bfc9d11d0a5665cdb3c9a28c9f232b65
Matthew Wigginton Conway <matt@indicatrix.org>
2014-10-06 13:39:22 -0400

    Use java.util.logging in the library. Fix #3.

--- gtfs-validation-lib/src/main/java/com/conveyal/gtfs/model/ValidationResult.java
+++ gtfs-validation-lib/src/main/java/com/conveyal/gtfs/model/ValidationResult.java
@@ -2,14 +2,16 @@ package com.conveyal.gtfs.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 
 public class ValidationResult {
-	
+	private static Logger _log = Logger.getLogger(ValidationResult.class.getName());
+			
 	public List<InvalidValue> invalidValues = new ArrayList<InvalidValue>();
 	
 	public void add(InvalidValue iv) {
-		System.out.println(iv);
+		_log.info(iv.toString());
 		invalidValues.add(iv);
 	}
 	
