286bebd1c5c3299f097f99f6d3e7a5291ee0ab85
Matthew Wigginton Conway <matt@indicatrix.org>
2014-10-09 17:43:34 -0400

    More ECMAScript 6 removal.

--- gtfs-validator-webapp/validation.js
+++ gtfs-validator-webapp/validation.js
@@ -202,7 +202,7 @@ $(document).ready(function () {
 	return;
     }
 
-    if (params.report.startsWith('//') || params.report.indexOf('://') != -1) {
+    if (params.report.indexOf('//') == 0 || params.report.indexOf('://') != -1) {
 	new ErrorView({
 	    model: new ErrorModel({title: 'Only local reports may be viewed', message: 'Please specify a local report to view'})
 	}).render().$el.appendTo('#content');
